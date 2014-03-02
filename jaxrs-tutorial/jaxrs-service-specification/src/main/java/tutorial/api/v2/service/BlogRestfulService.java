f8869778c9350a95985e4ca723d1e6d796a9435e
nazartm <>
2014-03-02 20:22:21 +0100

    Added support for default media types.

--- jaxrs-tutorial/jaxrs-service-specification/src/main/java/tutorial/api/v2/service/BlogRestfulService.java
+++ jaxrs-tutorial/jaxrs-service-specification/src/main/java/tutorial/api/v2/service/BlogRestfulService.java
@@ -10,11 +10,12 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/blog")
-@Consumes({"application/vnd.blog.v2+xml", "application/vnd.blog.v2+json"})
-@Produces({"application/vnd.blog.v2+xml", "application/vnd.blog.v2+json"})
+@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, "application/vnd.blog.v2+xml", "application/vnd.blog.v2+json"})
+@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, "application/vnd.blog.v2+xml", "application/vnd.blog.v2+json"})
 public interface BlogRestfulService {
 
     @GET
